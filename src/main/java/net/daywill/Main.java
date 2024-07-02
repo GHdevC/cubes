0f209b27429acd9523ce6899c20f4da2a1d1d71c
GHdevC <111580351+GHdevC@users.noreply.github.com>
2024-07-02 19:33:41 +0300

    Add files via upload

---
+++ src/main/java/net/daywill/Main.java
@@ -0,0 +1,201 @@
+package net.daywill;
+
+import net.daywill.item.Cube;
+import org.joml.Matrix4f;
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Main {
+
+    // The window handle
+    private long window;
+
+    private List<Cube> cubes = new ArrayList<>();
+    private Cube currentCube;
+
+    private boolean rotating = false;
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+
+        // Free the window callbacks and destroy the window
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(800, 600, "Cubes!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true);
+            if (action == GLFW_RELEASE || action == GLFW_REPEAT){
+                switch (key){
+                    case GLFW_KEY_W -> currentCube.y += 0.05f;
+                    case GLFW_KEY_S -> currentCube.y -= 0.05f;
+                    case GLFW_KEY_A -> currentCube.x -= 0.05f;
+                    case GLFW_KEY_D -> currentCube.x += 0.05f;
+                    case GLFW_KEY_LEFT_SHIFT -> currentCube.z -= 0.5f;
+                    case GLFW_KEY_SPACE -> currentCube.z += 0.5f;
+                    case GLFW_KEY_R -> rotating = !rotating;
+                    case GLFW_KEY_ENTER -> {
+                        cubes.add(currentCube);
+                        currentCube = new Cube(0f ,0f, -5f, 0f);
+                    }
+                }
+            }
+        });
+
+        currentCube = new Cube(0f, 0f, -5f, 0f);
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+
+    private void loop() {
+        GL.createCapabilities();
+
+        glEnable(GL_DEPTH_TEST);
+        Matrix4f projection = new Matrix4f().perspective((float) Math.toRadians(45.0),
+                800.0f / 600.0f, 0.1f, 100f);
+        FloatBuffer fb = BufferUtils.createFloatBuffer(16);
+        projection.get(fb);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( !glfwWindowShouldClose(window) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadMatrixf(fb);
+
+            for(Cube cube : cubes){
+                drawCube(cube);
+            }
+
+            drawCube(currentCube);
+            if(rotating) currentCube.angle += 0.5f;
+
+
+
+            glfwSwapBuffers(window); // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
+
+    private void drawCube(Cube cube){
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glTranslatef(cube.x, cube.y, cube.z);
+        glRotatef(cube.angle, 0f, 1f, 0f);
+        glScalef(0.33f, 0.33f, 0.33f);
+
+        glBegin(GL_QUADS);
+        glColor3f(1f, 0f, 0f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+
+        glColor3f(0f, 1f, 0f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+
+        glColor3f(0f, 0f, 1f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+
+        glColor3f(1f, 1f, 0f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+
+        glColor3f(1f, 1f, 1f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glColor3f(0f, 1f, 1f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+
+        glEnd();
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+}
---
+++ src/main/java/net/daywill/item/Cube.java
@@ -0,0 +1,14 @@
+package net.daywill.item;
+
+public class Cube {
+
+    public float x, y, z, angle;
+
+    public Cube(float x, float y, float z, float angle) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.angle = angle;
+    }
+
+}
